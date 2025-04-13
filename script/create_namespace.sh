4a71da61a0216c44a50590eebf873b6daa6ec262
Jamie Holland <jamie.holland@tripstax.com>
2025-04-13 17:05:21 +0100

    Secret PAT

--- script/create_namespace.sh
+++ script/create_namespace.sh
@@ -6,6 +6,7 @@ echo "Creating namespace $NAMESPACE..."
 if [ -n "$NAMESPACE" ]; then
   echo "Creating namespace $NAMESPACE"
   kubectl create ns "$NAMESPACE" || true
+  export NAMESPACE
 else
     printf "\n\n ERROR: NAMESPACE env var not set for k8s namespace creation."
     exit 1
